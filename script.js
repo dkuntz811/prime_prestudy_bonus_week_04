11e932a93a820229af637e852acd297462f57415
dkuntz811 <dkuntz811@gmail.com>
2016-07-09 11:38:49 -0500

    Week 4 Bonus .js

---
+++ script.js
@@ -0,0 +1,33 @@
+var theNumber = 1;
+ 
+var michelangelo = function(value){
+    value = (value * 2);
+    return value;
+}
+ 
+var donatello = function(value, anotherValue){
+    value = anotherValue + value;
+    anotherValue = value * anotherValue;
+    return anotherValue;
+}
+ 
+var leonardo = function(value){
+    var anotherValue = value;
+    value *= 4;
+    theNumber = value / 2;
+    return (value + anotherValue);
+}
+ 
+var raphael = function(value, anotherValue, yetAnotherValue){
+    value = value * anotherValue + yetAnotherValue;
+    return yetAnotherValue;
+}
+
+ function splinter (x, y) {
+    var z = x * y;
+    
+  var array = [x, y, z];  
+  return array; 
+ };
+ 
+console.log(splinter(1, 2)); 
